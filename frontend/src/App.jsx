dfbd2ddfddfff5ef205862b90446e4018f38af5d
{shivam <{shivamiit007@gmail.com}>
2025-12-25 20:47:14 +0530

    UI changes done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,8 @@ import axios from 'axios';
 import './App.css';
 
 // const API_URL = 'http://localhost:5000/api';
-const API_URL = `${process.env.REACT_APP_API_URL}/api`;
+// const API_URL = `${process.env.REACT_APP_API_URL}/api`;
+const API_URL = `${import.meta.env.VITE_API_URL ?? 'http://localhost:5000'}/api`;
 
 
 function App() {
